585321dbd989d758e8c1fb8d1192632ff834bcbd
dangkhoa0911228231-sketch <dangkhoa0911228231@gmail.com>
2026-06-16 14:09:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Tiêu đề ứng dụng
 st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 7_NGUYỄN THANH PHONG")
-
+st.image("LOGO.jpg")
 # Nhập dữ liệu
 C = st.number_input(
     "Nhập số tiền khách hàng gửi tiết kiệm (triệu đồng)",
